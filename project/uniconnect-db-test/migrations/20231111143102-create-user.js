754439ae96038e1dcbd3176ea5ed4ccc94c999fd
Huch0 <heochiyeong@gmail.com>
2023-11-16 12:25:24 +0900

    Modify User Table Schema - add createdAt, updatedAt attributes

--- project/uniconnect-db-test/migrations/20231111143102-create-user.js
+++ project/uniconnect-db-test/migrations/20231111143102-create-user.js
@@ -27,6 +27,15 @@ module.exports = {
           "banned"
         ),
       },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
       deletedAt: {
         type: Sequelize.DATE,
         allowNull: true,
