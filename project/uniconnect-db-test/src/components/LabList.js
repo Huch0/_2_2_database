0c78cc4a89d95798ae0223ce3d0e8ecef8f3a081
Huch0 <heochiyeong@gmail.com>
2023-11-26 19:52:04 +0900

    Show school_name in LabCard

--- project/uniconnect-db-test/src/components/LabList.js
+++ project/uniconnect-db-test/src/components/LabList.js
@@ -1,6 +1,14 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import fetchData from "@/utils/fetchData";
 
 function LabCard({ lab, selectedUser }) {
+  const [school, setSchool] = useState(null);
+
+  useEffect(() => {
+    if (!lab.school_id) return;
+    fetchData(`/api/school/${lab.school_id}`, setSchool);
+  }, [lab]);
+
   return (
     <div className="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-1 w-full flex-col justify-between">
       <div className="flex items-start px-4 py-6">
@@ -8,7 +16,10 @@ function LabCard({ lab, selectedUser }) {
           <h1 className="text-lg font-semibold">{lab.lab_name}</h1>
           <div className="flex items-center justify-between">
             <h2 className="text-lg text-gray-900 -mt-1">
-              {lab.school_id /* school_id를 school_name으로 바꾸세요. */}
+              {
+                school &&
+                  school.school_name /* school_id를 school_name으로 바꾸세요. */
+              }
             </h2>
           </div>
           <p className="mt-3 text-gray-700 text-sm">{lab.description}</p>
