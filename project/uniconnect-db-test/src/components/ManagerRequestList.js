cab2ee2741360d392c942cd3f7de740ab3b1546c
Huch0 <heochiyeong@gmail.com>
2023-11-26 19:48:28 +0900

    Fix Lab Manager Request Error

--- project/uniconnect-db-test/src/components/ManagerRequestList.js
+++ project/uniconnect-db-test/src/components/ManagerRequestList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import fetchData from "@/utils/fetchData";
 
 function RequestCard({ request }) {
   const handleConfirm = () => {};
@@ -8,17 +9,21 @@ function RequestCard({ request }) {
   const [lab, setLab] = useState(null);
 
   useEffect(() => {
+    // console.log(request, user, lab);
+    if (!request || !request.user_id || !request.lab_id) return;
     fetchData(`/api/user/${request.user_id}`, setUser);
     fetchData(`/api/lab/${request.lab_id}`, setLab);
-  }, []);
+  }, [request]);
 
   return (
     <div className="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-1 w-full flex-col justify-between">
       <div className="flex items-start px-4 py-6">
         <div className="">
-          <h1 className="text-lg font-semibold">{user.user_name}</h1>
+          <h1 className="text-lg font-semibold">{user && user.user_name}</h1>
           <div className="flex items-center justify-between">
-            <h2 className="text-lg text-gray-900 -mt-1">{lab.lab_name}</h2>
+            <h2 className="text-lg text-gray-900 -mt-1">
+              {lab && lab.lab_name}
+            </h2>
           </div>
         </div>
       </div>
